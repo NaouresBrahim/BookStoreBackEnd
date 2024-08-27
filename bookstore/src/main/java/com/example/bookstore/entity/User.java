beccf9d0d57eac9bcc9dfac01f7599d5c6e34bc3
nawre <naoures.brahim@cruxsoft.be>
2024-08-27 05:47:43 +0200

    Add database connection with h2 and add entities

--- bookstore/src/main/java/com/example/bookstore/entity/User.java
+++ bookstore/src/main/java/com/example/bookstore/entity/User.java
@@ -1,13 +1,27 @@
 package com.example.bookstore.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String username;
+    
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
 }
